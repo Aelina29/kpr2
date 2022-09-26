ff43106fc11a274a6eba49ba0420fb764e5d2ba7
Kondrateva Aelina <aelina.kondrateva@mail.ru>
2022-09-26 15:01:37 +0300

    done

--- src/cache.js
+++ src/cache.js
@@ -1,13 +1,28 @@
-class CacheValue{
-constructor(value, appeals) {value; appeals;}
-
-}
-
 class Cache{
-    constructor() { this._map = new Map() }
-    set (key, cValue)
+    constructor() { this._map = new Map(); this._stat = new Array(); }
+    set (key, val, rep = 1)
     {
-        this._map.set(key, cValue);
+        if (rep <=0) console.log("Количество обращений должно быть положительным!");//Точно надо ;??
+        else this._map.set(key, {'value':val, 'repeat':rep});
+    }
+    get (key)
+    {
+        if(!this._map.has(key)) return null;
+        this._stat.push(`${key} - ${this._map.get(key).value} (${this._map.get(key).repeat})`);
+        this._map.get(key).repeat -=1;
+        const val = this._map.get(key).value
+        if(this._map.get(key).repeat == 0) this._map.delete(key);
+        return val;
+    }
+    rep (key) //private
+    {
+        return this._map.get(key).repeat;
+    }
+    statistic ()
+    {
+        let st = new String();
+        this._stat.forEach(elem => st+= elem + "\n");
+        return st;
     }
 }
 
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,23 +1,114 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+test('1)null to unknown key', () => {
+    const c= new Cache();
+    expect(c.get("name")).toBeNull();
 });
 
-test('it pass', () => {
-    expect(false).toBe(false);
+test('2)right value by key', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    expect(c.get("name")).toEqual("John");//John; 3=>2
 });
+
+test('3)-1 repeat after get', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name"); //rep - private
+    expect(c.rep("name")).toEqual(2);//3=>2
+});
+
+test('4)-2 repeat after 2 get', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");//=>2
+    c.get("name");//=>1
+    expect(c.rep("name")).toEqual(1);//3=>2
+});
+
+test('5) delete, when repeat become 0 after 1 get', () => {
+    const c= new Cache();
+    c.set("name","John",1);
+    c.get("name");//=>0 and delete
+    expect(c.get("name")).toBeNull();//3=>2
+});
+
+test('6) delete, when repeat become 0 after 2 get', () => {
+    const c= new Cache();
+    c.set("name","John",2);
+    c.get("name");//=>1
+    c.get("name");//=>0 and delete
+    expect(c.get("name")).toBeNull();//3=>2
+});
+
+test('7) avto set 1 repeat', () => {
+    const c= new Cache();
+    c.set("name","John");
+    expect(c.rep("name")).toEqual(1);
+});
+
+test('8) delete, when repeat become 0 after 1 get (avto set repeat)', () => {
+    const c= new Cache();
+    c.set("name","John");
+    c.get("name");//=>0 and delete
+    expect(c.get("name")).toBeNull();//3=>2
+});
+
+test('9) not add negative repeat', () => {
+    const c= new Cache();
+    c.set("name","John",-3);
+    expect(c.get("name")).toBeNull();
+});
+
+test('10) simple statistic', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");
+    expect(c.statistic()).toEqual("name - John (3)\n");
+});
+
+test('11) statistic unknown key', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");
+    c.get("age");
+    expect(c.statistic()).toEqual("name - John (3)\n");
+});
+
+test('12) statistic 2 get', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");
+    c.get("name");
+    expect(c.statistic()).toEqual("name - John (3)\n"+"name - John (2)\n");
+});
+
+test('13) statistic 2 different get', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");
+    c.set("age","12",6);
+    c.get("age");
+    expect(c.statistic()).toEqual("name - John (3)\n"+"age - 12 (6)\n");
+});
+
+test('14) statistic set get set', () => {
+    const c= new Cache();
+    c.set("name","John",3);
+    c.get("name");
+    c.set("age","12",6);
+    expect(c.statistic()).toEqual("name - John (3)\n");
+});
+
+//статистика
+
 //=================================================
 
-describe('unknown key', () => {
-    it('should returns null if cache has no this key', () => {
-    c = new Cache();
-    expect(c.get("name")).toBeNull()
-    })
-})
-//одинаково
-test('unknown key', () => {
-    e  c = new Cache();
-    expect(c.get("name")).toBeNull()
-});
+//describe('unknown key', () => {
+//    it('should returns null if cache has no this key', () => {
+//   const c= new Cache();
+//    expect(c.get("name")).toBeNull()
+//    })
+//})
+
 
